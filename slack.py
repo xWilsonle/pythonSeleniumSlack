59b643d76bebc7b7232645ddd349e73fb2e27be7
xWilsonle <xwilsonle@gmail.com>
2019-01-17 09:06:22 -0800

    first commit

---
+++ slack.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+
+
+
+driver.get("https://slack.com/signin") #This is the website that will launch
