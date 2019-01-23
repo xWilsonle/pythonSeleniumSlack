a3f0ce0d5c3b590fa246ff0262dbb1640b16768d
xWilsonle <xwilsonle@gmail.com>
2019-01-23 09:23:26 -0800

    Logged into slack

---
+++ ReadMe.md
@@ -0,0 +1,12 @@
+Slack.py by Wilson Le
+
+I am going to launch slack, log in, and send a message
+
+I first find the domain input, clear the input, and type the server name
+Then I am going to find the login button and click it
+
+Then I am going to find the username field, and put my username
+Then I am going to find the password field, and put in my password
+I am then going to find the login button and click it
+
+I then find the text field, and type the message and click enter.
--- slack.py
+++ slack.py
@@ -9,3 +9,32 @@ driver = webdriver.Chrome()
 
 
 driver.get("https://slack.com/signin") #This is the website that will launch
+try:
+	#domain = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.ID, "domain")))
+	domain = driver.find_element_by_id('domain')
+	domain.clear()
+	domain.send_keys("test-60t7981.slack.com")
+
+	signin = driver.find_element_by_id("submit_team_domain")
+	signin.click()
+
+	email = driver.find_element_by_id('email')
+	password = driver.find_element_by_id('password')
+
+	email.clear()
+	password.clear()
+
+	email.send_keys("bloodypikachu@gmail.com")
+	password.send_keys('qatest123')
+
+	signinlogin = driver.find_element_by_id("signin_btn")
+	signinlogin.click()
+
+	msgbox = driver.find_element_by_xpath('//*[@id="msg_input"]/div[1]')
+	msgbox.click()
+	msgbox.send_keys("Hello peoples")
+	msgbox.send_keys(Keys.ENTER)
+
+
+finally:
+	print("fail")
